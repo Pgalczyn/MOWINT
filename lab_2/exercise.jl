77ed76ba6d2b41a566080b477362e9df9ad63871
Pgalczynski <pgalczynski@student.agh.edu.pl>
2025-03-20 11:04:35 +0100

    lab2

--- lab_2/exercise.jl
+++ lab_2/exercise.jl
@@ -11,17 +11,14 @@ println(y)
 
 
 #zadanie 2
-
 results = []
 
 for i in 1:10000
-
-    push!(results,eps(Float64(i)))
-
+    push!(results, eps(Float64(i)))
 end
 
 using Plots
-plot(1:10000,results)
+plot(1:10000, results, xlabel="Wartość liczby", ylabel="Odległość (eps)", title="Zmiana odległości między liczbami zmiennoprzecinkowymi", label="eps(x)")
 
 
 #zadanie 3
@@ -37,5 +34,42 @@ for num in numbers
     println(decode(num), issubnormal(num))  
 end
 
+#zadanie 4
+function count_expencional(numberOfElements,x)
+    result::Float16 = 0
+
+    for i=0:numberOfElements
+
+        result += (x^i)/factorial(big(i))
 
 
+    end
+
+    return result
+
+    
+end
+
+
+println("przybliżenie policzone algorytmem niestabilnym numerycznie: ", count_expencional(40,-5.5))
+println("prawdziwa wartość: ",exp(-5.5))
+println("różnica: ", exp(-5.5) - count_expencional(40,-5.5))
+println("błąd: ", (count_expencional(40,-5.5) - exp(-5.5))/exp(-5.5)," %")
+
+function count_expencional_better(numberOfElements,x)
+    result::Float16 = 0
+
+    for i=0:numberOfElements
+
+            result += (x^i)/factorial(big(i))
+
+    end
+
+    return 1/result
+    
+end
+
+println("przybliżenie policzone algorytmem stabilnym numerycznie: ", count_expencional_better(30,5.5))
+println("prawdziwa wartość: ",exp(-5.5))
+println("różnica: ", exp(-5.5) - count_expencional_better(30,5.5))
+println("błąd: ", (exp(-5.5) - count_expencional_better(40,5.5))/exp(-5.5)," %")
